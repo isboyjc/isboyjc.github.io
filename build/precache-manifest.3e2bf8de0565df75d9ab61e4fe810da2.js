0940bc82b5264332564f76f2d61c57de5f4e908b
IsboyJC <214930661@qq.com>
2019-11-07 10:01:58 +0800

    Add files via upload

---
+++ build/precache-manifest.3e2bf8de0565df75d9ab61e4fe810da2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62d721aae9855f0f7db1c29ba7f1a7f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a27ca6f76c9e69c6cccb",
+    "url": "./static/css/main.999db9a5.chunk.css"
+  },
+  {
+    "revision": "3b046fd11dc415ded852",
+    "url": "./static/js/2.eee47c97.chunk.js"
+  },
+  {
+    "revision": "a27ca6f76c9e69c6cccb",
+    "url": "./static/js/main.d7010478.chunk.js"
+  },
+  {
+    "revision": "13bd100f5be9ce2b027a",
+    "url": "./static/js/runtime~main.92dc2f97.js"
+  },
+  {
+    "revision": "db7ddd5a4c6bcd2cbe349110b51daaeb",
+    "url": "./static/media/avatar.db7ddd5a.png"
+  },
+  {
+    "revision": "3165c186e5879ca5ee9dfd119953d245",
+    "url": "./static/media/icon-audience-inactived.3165c186.png"
+  },
+  {
+    "revision": "2620e52585f92615e6ab5ee899c119f6",
+    "url": "./static/media/icon-audience.2620e525.png"
+  },
+  {
+    "revision": "25174b6ec7aee7a101e68cdf6d7143a8",
+    "url": "./static/media/icon-host-inactived.25174b6e.png"
+  },
+  {
+    "revision": "6baf9f5c788826fe3e42804dec3b89ab",
+    "url": "./static/media/icon-host.6baf9f5c.png"
+  },
+  {
+    "revision": "41c0dce84904b9e62385f51a888a73c1",
+    "url": "./static/media/logo-open-live-gray.41c0dce8.png"
+  },
+  {
+    "revision": "1cccf552e25f6614e1b734b29eb4b94d",
+    "url": "./static/media/logo-open-live.1cccf552.png"
+  }
+]);
